31def69b1dc3265339deeedbe7facacef35f3b5f
Symphony <95200261@qq.com>
2019-11-07 11:34:05 +0800

    使用2种方式绘制圆角矩形

--- app/src/main/java/com/symphony/customcontrol/view/TestView.kt
+++ app/src/main/java/com/symphony/customcontrol/view/TestView.kt
@@ -1,7 +1,10 @@
 package com.symphony.customcontrol.view
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 
@@ -46,18 +49,37 @@ class TestView: View {
         //canvas.drawLine(300f,300f,500f,600f,mPaint)
         //canvas.drawLines(floatArrayOf(100f,200f,200f,200f,100f,300f,200f,300f),mPaint)
         /**
-         * 绘制矩形  确定一个矩形最少需要四个数据，就是对角线的两个点的坐标值，这里一般采用左上角和右下角的两个点的坐标
+         * 绘制矩形
+         *
+         * drawRect  或者搭配 Rect 或者搭配 RectF
+         *
+         * 确定一个矩形最少需要四个数据，就是对角线的两个点的坐标值，这里一般采用左上角和右下角的两个点的坐标
          *
          * Rect是int(整形)的，而RectF是float(单精度浮点型)的
          */
         //1.第一种
-        canvas.drawRect(100f,100f,800f,400f,mPaint)
+        //canvas.drawRect(100f,100f,800f,400f,mPaint)
         //2.第二种
-        val rect = Rect(100,100,800,400)
-        canvas.drawRect(rect,mPaint)
+        /*val rect = Rect(100,100,800,400)
+        canvas.drawRect(rect,mPaint)*/
         //3.第三种
+        /*val rectF = RectF(100f,100f,800f,400f)
+        canvas.drawRect(rectF,mPaint)*/
+        /**
+         * 绘制圆角矩形
+         *
+         * drawRoundRect
+         *
+         * 第二种方法在API21的时候才添加上，所以我们一般使用的都是第一种。
+         *
+         * 圆角矩形的角实际上不是一个正圆的圆弧，而是椭圆的圆弧，这里的两个参数实际上是椭圆的两个半径
+         */
+        //1.第一种
         val rectF = RectF(100f,100f,800f,400f)
-        canvas.drawRect(rectF,mPaint)
+        canvas.drawRoundRect(rectF,30f,30f,mPaint)
+        //2.第二种
+        canvas.drawRoundRect(100f,100f,800f,400f,30f,30f,mPaint)
+
     }
 
 }
